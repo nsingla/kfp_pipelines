bde8795e7eec30c28dc124dcc771b279905d841c
Nelesh Singla <117123879+nsingla@users.noreply.github.com>
2025-06-25 11:03:44 -0400

    making the tests work for more pipeline combos
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    Signed-off-by: Nelesh Singla <117123879+nsingla@users.noreply.github.com>

--- backend/test/v2/api/utils/test_utils.go
+++ backend/test/v2/api/utils/test_utils.go
@@ -1,9 +1,24 @@
 package test
 
+import (
+	"math/rand"
+	"time"
+)
+
 func ParsePointersToString(s *string) string {
 	if s == nil {
 		return ""
 	} else {
 		return *s
 	}
+}
+
+func GetRandomString(length int) string {
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
