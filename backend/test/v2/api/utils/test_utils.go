3887a3ec1cbc35da64e0c786b1e46f5515956655
Nelesh Singla <117123879+nsingla@users.noreply.github.com>
2025-06-27 16:42:57 -0400

    adding go docs
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- backend/test/v2/api/utils/test_utils.go
+++ backend/test/v2/api/utils/test_utils.go
@@ -1,3 +1,17 @@
+// Package test
+// Copyright 2018-2023 The Kubeflow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package test
 
 import (
@@ -5,6 +19,7 @@ import (
 	"time"
 )
 
+// ParsePointersToString - convert a string pointer to string value
 func ParsePointersToString(s *string) string {
 	if s == nil {
 		return ""
@@ -13,6 +28,7 @@ func ParsePointersToString(s *string) string {
 	}
 }
 
+// GetRandomString - Get a random string of length x
 func GetRandomString(length int) string {
 	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
