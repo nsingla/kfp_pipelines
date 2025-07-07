acc90a1ff81208ea7ea1b8ca63acbb28b68a1890
Nelesh Singla <117123879+nsingla@users.noreply.github.com>
2025-07-07 14:13:40 -0400

    renaming report files
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    Signed-off-by: Nelesh Singla <117123879+nsingla@users.noreply.github.com>

--- backend/test/v2/api/integration_suite_test.go
+++ backend/test/v2/api/integration_suite_test.go
@@ -47,8 +47,8 @@ var pipelineUploadParams *upload_params.UploadPipelineParams
 var pipelineGeneratedName string
 var testLogsDirectory = "logs"
 var testReportDirectory = "reports"
-var junitReportFilename = "junit-api.xml"
-var jsonReportFilename = "report-api.json"
+var junitReportFilename = "api.xml"
+var jsonReportFilename = "api.json"
 
 var _ = BeforeSuite(func() {
 	err := os.MkdirAll(testLogsDirectory, 0755)
